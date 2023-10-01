91701440dce9b9bdff24d25d25b00f2b6c6b593a
Harsh Virani <iamharshvirani7@gmail.com>
2023-10-01 11:45:19 -0700

    Initial setup and connection to database

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2" 
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := gorm.Open(mysql.Open("root:root12345@/go-auth-react-jwt"), &gorm.Config{})
+
+	if err != nil { 
+		panic("could not connect to the database ")
+	}
+
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World 👋!")
+    })
+
+    app.Listen(":8000")
+}
